8f10ba1d1ebdbe0b4bad0d7f55640818b232747c
thangpv <thangpv663251@ut.edu.vn>
2026-08-22 00:20:32 +0700

    chore: remove syntax fix script

--- fix-syntax.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const file = 'modules/enrollment/services/EnrollmentApplicationService.js';
-let code = fs.readFileSync(file, 'utf8');
-code = code.replace(/_body: \(\{([\s\S]*?)\}\);/g, '_body: {$1} };');
-fs.writeFileSync(file, code);
-console.log('Fixed syntax errors');
